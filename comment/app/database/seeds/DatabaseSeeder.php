2453e24ad0ed53ca51ca093acc1722da003357a7
amichels <andy.j.michels@gmail.com>
2014-09-17 21:07:24 -0500

    backend done

--- comment/app/database/seeds/DatabaseSeeder.php
+++ comment/app/database/seeds/DatabaseSeeder.php
@@ -10,7 +10,9 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-
+		
+		$this->call('CommentTableSeeder');
+		$this->command->info('Comment table seeded.');
 		// $this->call('UserTableSeeder');
 	}
 
